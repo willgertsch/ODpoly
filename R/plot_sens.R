efde30d36d534ee004462db22d62878a2312da76
Will Gertsch <willgertsch+github@gmail.com>
2021-12-23 10:00:59 -0800

    Replaced base R plot with ggplot.

--- R/plot_sens.R
+++ R/plot_sens.R
@@ -8,6 +8,16 @@ plot_sens = function(xvals, vars, betas, powers) {
   # compute sens function
   yvals = sapply(xs, sens, vars, betas, powers)
   
-  plot(yvals ~ xvals, type ="l")
-  abline(h=0)
+  # old base R plots
+  # plot(yvals ~ xvals, type ="l")
+  # abline(h=0)
+  
+  # replaced with ggplot because can return plot object
+  library(ggplot2)
+  p = ggplot(mapping = aes(y = yvals, x = xvals)) + 
+    geom_line(color = "blue") + 
+    geom_hline(yintercept = 0) +
+    theme_bw()
+  
+  return(p)
 }
