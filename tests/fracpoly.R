8459bf964c7276a9bd2fe5b6c8218811d7989831
Will Gertsch <willgertsch+github@gmail.com>
2021-12-23 10:00:29 -0800

    Added file for testing functional shape of fractional poly. Not all that useful. Developed a separate shiny app for this that will be part of the eventual UI.

--- tests/fracpoly.R
+++ tests/fracpoly.R
@@ -24,3 +24,25 @@ ys = betas[1] + betas[2] * x1 + betas[3] * x2
 
 # plot
 plot(ys~xs, type = "l")
+
+for (i in seq(-1,1,0.1)) {
+  betas = c(i, i, i)
+  # compute x terms
+  if (powers[1] == powers[2]) { # apply correct x function if repeated power
+    if (sum(xs<=0)>1) {
+      print("Fractional polynomials are only defined on (0, inf]")
+    }
+    
+    x1 = xs^powers[1]
+    x2 = log(xs) * xs^powers[1]
+  } else {
+    x1 = xs^powers[1]
+    x2 = xs^powers[2]
+  }
+  
+  
+  # compute y
+  ys = betas[1] + betas[2] * x1 + betas[3] * x2
+  
+  lines(ys ~ xs) # additional lines
+}
