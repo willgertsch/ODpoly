0ec500b67ae1c27dc414389794b14ad31adb6ec2
Will Gertsch <willgertsch+github@gmail.com>
2022-01-31 16:24:19 -0800

    moved function to its own file

--- R/app.R
+++ R/app.R
@@ -9,18 +9,6 @@ metaheuristics = c("Particle Swarm Optimization",
                    "Moth Flame Optimizer",
                    "Differential Evolution")
 
-metaheur_dict = function(fullname){
-  if (fullname == "Particle Swarm Optimization")
-    return("PSO")
-  else if (fullname == "Grey Wolf Optimizer")
-    return("GWO")
-  else if (fullname == "Harmony Search Algorithm")
-    return("HS")
-  else if (fullname == "Moth Flame Optimizer")
-    return("MFO")
-  else if (fullname == "Differential Evolution")
-    return("DE")
-}
 
 ODpolyApp <- function(...) {
   ui <- fixedPage(
---
+++ R/metaheur_dict.R
@@ -0,0 +1,14 @@
+# metaheur_dict.R
+# dictionary function for metaheuristics
+metaheur_dict = function(fullname){
+  if (fullname == "Particle Swarm Optimization")
+    return("PSO")
+  else if (fullname == "Grey Wolf Optimizer")
+    return("GWO")
+  else if (fullname == "Harmony Search Algorithm")
+    return("HS")
+  else if (fullname == "Moth Flame Optimizer")
+    return("MFO")
+  else if (fullname == "Differential Evolution")
+    return("DE")
+}
